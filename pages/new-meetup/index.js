64d1567424022c74c40b0bada7975de2ac83e8c3
rawwr <akshayace0302@gmail.com>
2022-07-10 00:32:26 +0530

    adding the new meetup form

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,1 +1,12 @@
 //ourdomain.com/new-meetup.js
+import NewMeetupForm from "../../components/meetups/NewMeetupForm";
+
+function NewMeetupPage() {
+  const addMeetupHandler = (enteredData) => {
+    console.log(enteredData);
+  };
+
+  return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
+}
+
+export default NewMeetupPage;
